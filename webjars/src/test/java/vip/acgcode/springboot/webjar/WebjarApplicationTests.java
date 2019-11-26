27e78f725500278642f0d61738d327c890a1a266
CodeMonsterM <codemonsterm@163.com>
2019-11-27 01:17:27 +0800

    初步使用WebJar

---
+++ webjars/src/test/java/vip/acgcode/springboot/webjar/WebjarApplicationTests.java
@@ -0,0 +1,13 @@
+package vip.acgcode.springboot.webjar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebjarApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
